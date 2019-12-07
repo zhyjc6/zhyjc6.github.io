62d7f78a3aaa3a4aa37fe4ff259f78116bd7bfe9
zhyjc6 <zhyjc6@qq.com>
2019-12-07 23:00:13 +0800

    js、css cdn化,改动文件引用布局

--- static/js/plugins.js
+++ static/js/plugins.js
@@ -9,15 +9,3 @@ var _mtac = {};
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(mta, s);
 })();
-
-/* 预计阅读时间 */
-var text = $('.post').text().length;
-var text_num = read_time/400;
-var read_time = Math.round(read_time);
-alert(text);
-if(read_time>1){
-        $('#read-time').text('预计阅读时间'+read_time+'分钟');
-}else{
-        $('#read-time').text('预计阅读时间 1 分钟');
-}
-
